d6e55605c92be42aa9952b976897b33e2c76af8b
Kampfkrapfn <github@14m1337.de>
2017-07-28 15:27:33 +0200

    Create deDE.lua

---
+++ ChatSuey-Timestamps/i18n/deDE.lua
@@ -0,0 +1,6 @@
+local LS = ChatSuey.Locales["deDE"].Strings;
+
+LS["Timestamps"] = "Zeitstempel";
+LS["Use 24 hour clock"] = "24-Stunden-Format benutzen";
+LS["Include seconds"] = "Sekunden anzeigen";
+LS["Use consistent color"] = "immer dieselbe Farbe benutzen";
