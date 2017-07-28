a98efc0346198bd4fbbe5156d92ca807f46c1316
Kampfkrapfn <github@14m1337.de>
2017-07-28 15:24:50 +0200

    Create deDE.lua

---
+++ ChatSuey-FontSettings/i18n/deDE.lua
@@ -0,0 +1,9 @@
+local LS = ChatSuey.Locales["deDE"].Strings;
+
+LS["Font"] = "Schriftart";
+LS["Size"] = "Größe";
+LS["Outline"] = "Umrandung";
+LS["None"] = "Nichts";
+LS["Thin"] = "Dünn";
+LS["Thick"] = "Dick";
+LS["Family"] = "Familie";
