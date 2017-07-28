e2fd874b25cbf35a72007ec3c8959211eeff50ec
Kampfkrapfn <github@14m1337.de>
2017-07-28 15:26:45 +0200

    Create deDE.lua

---
+++ ChatSuey-MiniChannels/i18n/deDE.lua
@@ -0,0 +1,10 @@
+local LS = ChatSuey.Locales["deDE"].Strings;
+
+LS["P"] = "GR";     -- Party (Gruppe)
+LS["R"] = "R";      -- Raid
+LS["G"] = "G";      -- Guild
+LS["O"] = "O";      -- Officer
+LS["RL"] = "SL";    -- Raid Leader (Schlachtzugsleiter)
+LS["RW"] = "SW";    -- Raid Warning (Schlachtzug)
+LS["BG"] = "SF";    -- Battleground (Schlachtfeld)
+LS["BL"] = "SFL";   -- Battleground Leader (Schlachtfeldleiter)
